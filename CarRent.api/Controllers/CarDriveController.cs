6a6c87ff25497228c2cd1e8b62de0326eca957af
Mateusz Serafin <mateusz97.serafin@gmail.com>
2023-10-07 22:44:27 +0200

    compleated CarDriveController

--- CarRent.api/Controllers/CarDriveController.cs
+++ CarRent.api/Controllers/CarDriveController.cs
@@ -1,4 +1,5 @@
-﻿using CarRent.Repository;
+﻿using CarRent.data.DTO;
+using CarRent.Repository;
 using CarRent.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,8 +8,49 @@ namespace CarRent.api.Controllers
     [Route("[controller]")]
     public class CarDriveController : BaseController
     {
-        public CarDriveController(IServiceManager service) : base(service)
+        public CarDriveController(IServiceManager service)
+            : base(service)
         {
+
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetCarDrives()
+        {
+            var list = await _services.CarDriveService.GetAllAsync(false);
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetActiveCarDrives()
+        {
+            var list = await _services.CarDriveService.GetAllActiveAsync(false);
+            return Ok(list);
+        }
+
+        [HttpGet("get/{id:int}")]
+        public async Task<IActionResult> GetCarDriveById(int id)
+        {
+            var carDrive = await _services.CarDriveService.GetAsync(id, false); 
+            return Ok(carDrive);
+        }
+
+        public async Task<IActionResult> CreateCarDrive([FromBody] CarDriveDto carDrive)
+        {
+            var createdCarDrive = await _services.CarDriveService.CreateAsync(carDrive);
+            return CreatedAtAction(nameof(CreateCarDrive), new { createdCarDrive.Id, createdCarDrive });
+        }
+
+        public async Task<IActionResult> UpdateCarDrive(int id, [FromBody] CarDriveDto carDrive)
+        {
+            await _services.CarDriveService.UpdateAsync(id, carDrive, trackChanges: true);
+
+            return NoContent();
+        }
+
+        public Task<IActionResult> DeleteCarDrive(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
