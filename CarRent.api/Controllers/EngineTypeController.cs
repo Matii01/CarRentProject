a4436ae7dd6321dbdc1f601f6b836ead1c7d59c4
Mateusz Serafin <mateusz97.serafin@gmail.com>
2023-10-07 22:51:50 +0200

    Added EngineTypeController

---
+++ CarRent.api/Controllers/EngineTypeController.cs
@@ -0,0 +1,54 @@
+﻿using CarRent.data.DTO;
+using CarRent.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRent.api.Controllers
+{
+    public class EngineTypeController : BaseController
+    {
+        public EngineTypeController(IServiceManager serviceManager)
+            : base(serviceManager)
+        {
+        
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllEngineTypes()
+        {
+            var list = await _services.EngineTypeService.GetAllAsync(false);
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetActiveEngineTypes()
+        {
+            var list = await _services.EngineTypeService.GetAllAsync(false);
+            return Ok(list);
+        }
+
+        [HttpGet("get/{id:int}")]
+        public async Task<IActionResult> GetEngineTypesById(int id)
+        {
+            var engineType = await _services.EngineTypeService.GetAsync(id, false);
+            return Ok(engineType);
+        }
+
+        public async Task<IActionResult> CreateEngineType([FromBody] EngineTypeDto engineType)
+        {
+            var createdEngineType = await _services.EngineTypeService.CreateAsync(engineType);
+            return CreatedAtAction(nameof(CreateEngineType), new { createdEngineType.Id, engineType });
+        }
+
+        public async Task<IActionResult> UpdateCarDrive(int id, [FromBody] EngineTypeDto EngineType)
+        {
+            await _services.EngineTypeService.UpdateAsync(id, EngineType, trackChanges: true);
+
+            return NoContent();
+        }
+
+        public Task<IActionResult> DeleteEngineType(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
