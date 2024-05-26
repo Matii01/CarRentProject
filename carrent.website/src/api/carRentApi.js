5da12588a31e0bff5831d9c1b78c03b357a8a7f6
Mateusz Serafin <mateusz97.serafin@gmail.com>
2024-05-26 11:50:51 +0200

    Added authorization token to header

--- carrent.website/src/api/carRentApi.js
+++ carrent.website/src/api/carRentApi.js
@@ -2,6 +2,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import config from "../../config";
 
 export const carRentApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: config.API_URL }), // config.API_URL
+  baseQuery: fetchBaseQuery({
+    baseUrl: config.API_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: () => ({}),
 });
